5ebb8f11e43a5e6d5ba074f169357c4e02aa29b0
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-04-08 07:06:06 -0500

    added notes and worked on calcdescription

--- 02-basics/assets/scripts/app.js
+++ 02-basics/assets/scripts/app.js
@@ -3,6 +3,8 @@
 
 let currentResult = 0;
 
-currentResult = currentResult + 10;
+let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1';
 
-outputResult(currentResult, '');
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+outputResult(currentResult, calculationDescription);
