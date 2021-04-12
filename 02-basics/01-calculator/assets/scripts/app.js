2de55f72a8e43a55094b2c58c24ba08cffa2dfc7
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-04-12 04:55:05 -0500

    assignment 2 done

--- 02-basics/01-calculator/assets/scripts/app.js
+++ 02-basics/01-calculator/assets/scripts/app.js
@@ -3,14 +3,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
 }
 
-currentResult = add(5, 10);
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-let errorMessage = 'An Error \n' + 'Occurred';
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
