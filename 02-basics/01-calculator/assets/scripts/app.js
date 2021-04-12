0db60254c30069aed97e6f8d721d67a413e8c279
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-04-12 05:19:38 -0500

    addition function now works and updated notes

--- 02-basics/01-calculator/assets/scripts/app.js
+++ 02-basics/01-calculator/assets/scripts/app.js
@@ -4,7 +4,7 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
+  currentResult = currentResult + parseInt(userInput.value);
   outputResult(currentResult, '');
 }
 
