2ebea9252c145594baa8b7127081419a1bc09591
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-04-12 03:51:03 -0500

    returning result

--- 02-basics/01-calculator/assets/scripts/app.js
+++ 02-basics/01-calculator/assets/scripts/app.js
@@ -5,13 +5,10 @@ let currentResult = defaultResult;
 
 function add(num1, num2) {
   const result = num1 + num2;
-  alert(`The result is ${result}`);
   return result;
 }
 
-add(5, 10);
-
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+currentResult = add(5, 10);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 let errorMessage = 'An Error \n' + 'Occurred';
