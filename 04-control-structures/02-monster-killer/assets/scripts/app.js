2bc89b81c41ae0bea7cf523d03df2ea288d8da60
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-04-18 23:06:45 -0500

    player can now choose the life

--- 04-control-structures/02-monster-killer/assets/scripts/app.js
+++ 04-control-structures/02-monster-killer/assets/scripts/app.js
@@ -3,7 +3,14 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Maximum life for you and the monster.', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
@@ -40,7 +47,7 @@ function endRound() {
 
   if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0) {
     alert(
-      'Game is reseting! If you used your BONUS LIFE at any time that will not reset'
+      'Game is reseting! If you used your BONUS LIFE at any time that will not reset.'
     );
     reset();
   }
