1376a9969be6b22766576c4e9fd653ca9068f384
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-04-25 20:53:40 -0500

    06 started

---
+++ 06-more-on-functions/01-jan/app.js
@@ -0,0 +1,17 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+
+startGameBtn.addEventListener('click', startGame);
+
+// Functions in objects are called methods.
+// const person = {
+//   name: 'Josh'
+//   greet: function greet() {
+//     console.log('Hello there!');
+//   }
+// };
+
+// person.greet();
