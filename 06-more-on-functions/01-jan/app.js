d5fd9726e3e7c467dd0977bffef08dddf487a6b2
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-04-28 07:16:27 -0500

    start game is functioning

--- 06-more-on-functions/01-jan/app.js
+++ 06-more-on-functions/01-jan/app.js
@@ -1,5 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameisRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}`, '').toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener('click', function startGame() {
+  if (gameisRunning) {
+    return;
+  }
+  gameisRunning = true;
+  console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
+
 // const start = function () {
 //   console.log('Game is starting...');
 // };
@@ -21,7 +47,3 @@ const startGameBtn = document.getElementById('start-game-btn');
 // };
 
 // person.greet();
-
-startGameBtn.addEventListener('click', function startGame() {
-  console.log('Game is starting...');
-});
