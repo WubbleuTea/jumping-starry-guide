563d9fec295816ebbef9f61be4e565ab1830e1b2
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-05-17 07:06:03 -0500

    added Dom manipulation

---
+++ 07-working-with-dom/01-/app.js
@@ -0,0 +1,21 @@
+const h1 = document.getElementById('main-title');
+
+h1.textContent = 'Some new title!';
+h1.style.color = 'white';
+h1.style.backgroundColor = 'black';
+
+const li = document.querySelector('li:last-of-type');
+
+li.textContent = `${li.textContent} Changed!`;
+
+const body = document.body;
+
+// Does not give live changes to the elements (# of items in array)
+// const listItemElements = document.querySelectorAll('li');
+
+// Gives live changes to the element (# of items in array)
+const listItemElements = document.getElementsByTagName('li');
+
+for (const listItemEl of listItemElements) {
+  console.dir(listItemEl);
+}
