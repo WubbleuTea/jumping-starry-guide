67c2d2abfa026d202166cbd12f40050d7c8adfe5
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-05-18 07:09:05 -0500

    assignment1 done

---
+++ 07-working-with-dom/99-01-assignment-1/assignment.js
@@ -0,0 +1,14 @@
+const taskOne = document.getElementById('task-1');
+const taskOneSecond = document.querySelector('#task-1');
+const titleOne = document.querySelector('title');
+const titleTwo = document.head.querySelector('title');
+const h1 = document.querySelector('h1');
+
+taskOne.style.backgroundColor = 'black';
+taskOneSecond.style.color = 'white';
+
+titleOne.innerText = 'Assignment - Solved!';
+
+h1.innerText = 'Assignment - Solved!';
+
+console.dir(document);
