82a49513d14eed21c8ada82e074424538f4ac0d9
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-05-23 20:54:04 -0500

    added notes, classes and used classlist

--- 07-working-with-dom/01-/app.js
+++ 07-working-with-dom/01-/app.js
@@ -3,6 +3,24 @@ const firstLi = ul.firstElementChild;
 
 console.log(firstLi);
 
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+// section.style.backgroundColor = 'blue';
+
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+  // if (section.className === 'red-bg visable') {
+  //   section.className = 'red-bg invisible';
+  // } else {
+  //   section.className = 'red-bg visable';
+  // }
+
+  section.classList.toggle('visible');
+  section.classList.toggle('invisible');
+});
+
 // const h1 = document.getElementById('main-title');
 
 // h1.textContent = 'Some new title!';
