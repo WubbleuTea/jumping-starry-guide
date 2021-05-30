a97a6232375422c3ded22950f1a60fab0e410375
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-05-29 23:42:35 -0500

    backdrop and cancel work

--- 07-working-with-dom/02-movies/assets/scripts/app.js
+++ 07-working-with-dom/02-movies/assets/scripts/app.js
@@ -4,3 +4,29 @@ const addMovieModal = document.getElementById('add-modal');
 
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
+
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild;
+
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
