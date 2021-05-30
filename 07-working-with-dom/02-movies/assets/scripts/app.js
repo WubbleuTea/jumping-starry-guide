4572e24679374025d2b5407b12b045008cb744ca
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-05-30 00:05:50 -0500

    movies are added

--- 07-working-with-dom/02-movies/assets/scripts/app.js
+++ 07-working-with-dom/02-movies/assets/scripts/app.js
@@ -9,6 +9,12 @@ const backdrop = document.getElementById('backdrop');
 // const backdrop = document.body.firstElementChild;
 
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButtonn = cancelAddMovieButton.nextElementSibling;
+
+const userInputs = addMovieModal.querySelectorAll('input');
+// const userInputs = addMovieModal.getElementsByTagName('input');
+
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -19,7 +25,34 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue === '' ||
+    ratingValue === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please enter valid values (ratnig between 1 and 5');
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
   toggleMovieModal();
 };
 
@@ -29,4 +62,5 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButtonn.addEventListener('click', addMovieHandler);
