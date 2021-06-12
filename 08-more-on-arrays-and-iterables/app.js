9c2f27b9a21e8e287f6f332a43b1441a724b1173
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-06-11 23:53:44 -0500

    arrays are made

--- 08-more-on-arrays-and-iterables/app.js
+++ 08-more-on-arrays-and-iterables/app.js
@@ -1,1 +1,19 @@
 const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = new Array('Hi', 'welcome'); // []
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
+
+// // Turns iterables into arrays
+// const moreNumbers = Array.from('Hi!');
+// console.log(moreNumbers);
+
+const listItems = document.querySelectorAll('li');
+
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);
