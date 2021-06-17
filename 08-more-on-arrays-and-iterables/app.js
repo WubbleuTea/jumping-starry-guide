bbd9b21e769c5363b752c7ce666ac1411b5ad7e3
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-06-17 17:00:20 -0500

    added more slice elements

--- 08-more-on-arrays-and-iterables/app.js
+++ 08-more-on-arrays-and-iterables/app.js
@@ -36,46 +36,68 @@
 //   }
 // }
 
-// console.log(personalData[0]);
+// // console.log(personalData[0]);
 
-// ADD AND REMOVING ELEMENTS
-const hobbies = ['Cooking', 'Sports'];
-console.log(hobbies);
+// // ADD AND REMOVING ELEMENTS
+// const hobbies = ['Cooking', 'Sports'];
+// console.log(hobbies);
 
-// Adds item to end of array
-// Returns a number which is the length of the array
-hobbies.push('Reading');
-// Adds item to beginning of array.
-// Returns a number which is the length of the array
-hobbies.unshift('Coding');
-// Removes the last element from an Array
-// // hobbies.pop()
-// Can save the value that is being popped in a variable
-const poppedValue = hobbies.pop();
-// Removes the first element from an Array
-hobbies.shift();
+// // Adds item to end of array
+// // Returns a number which is the length of the array
+// hobbies.push('Reading');
+// // Adds item to beginning of array.
+// // Returns a number which is the length of the array
+// hobbies.unshift('Coding');
+// // Removes the last element from an Array
+// // // hobbies.pop()
+// // Can save the value that is being popped in a variable
+// const poppedValue = hobbies.pop();
+// // Removes the first element from an Array
+// hobbies.shift();
 
-console.log(hobbies);
+// console.log(hobbies);
 
-hobbies[1] = 'CHANGED';
-// // if you hard code a change to an index not in the current arry it will insert it where you want to but add empty elements into the array
-// hobbies[5] = 'FIFTH';
+// hobbies[1] = 'CHANGED';
+// // // if you hard code a change to an index not in the current arry it will insert it where you want to but add empty elements into the array
+// // hobbies[5] = 'FIFTH';
 
-console.log(hobbies, hobbies[4]);
+// console.log(hobbies, hobbies[4]);
 
-// at index 1 delete none and insert the third argument to become the new index that you put in the first argument.
-hobbies.splice(1, 0, 'Good Food');
-console.log('spliced hobbies', hobbies);
+// // at index 1 delete none and insert the third argument to become the new index that you put in the first argument.
+// hobbies.splice(1, 0, 'Good Food');
+// console.log('spliced hobbies', hobbies);
 
-//splce returns the removed elements
-const removedElements = hobbies.splice(0, 1);
-console.log('spliced deleted hobbies', hobbies);
-console.log('removed elements ', removedElements);
+// //splce returns the removed elements
+// const removedElements = hobbies.splice(0, 1);
+// console.log('spliced deleted hobbies', hobbies);
+// console.log('removed elements ', removedElements);
 
-//if you use negative numbers as the index number splice will look from the end and not the beginning
-const removedElementsNegative = hobbies.splice(-1, 1);
-console.log('spliced deleted negative hobbies', hobbies);
-console.log('removed elements negative', removedElementsNegative);
+// //if you use negative numbers as the index number splice will look from the end and not the beginning
+// const removedElementsNegative = hobbies.splice(-1, 1);
+// console.log('spliced deleted negative hobbies', hobbies);
+// console.log('removed elements negative', removedElementsNegative);
 
-// // splice without a delete count it deletes that item and all the items after the index number.
-// hobbies.splice(1)
+// // // splice without a delete count it deletes that item and all the items after the index number.
+// // hobbies.splice(1)
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// // both values are changed because arrays are reference values and are updated
+// const storedResults = testResults;
+// // however if you slice the array then it will only ever reference the original array
+// // slice with no values give you the full array
+
+// const storedResults = testResults.slice();
+// // you can include numbers in slice to take a certain range
+// // can use negative intergers
+
+// const storedResults = testResults.slice(0, 2);
+
+// if you only put one number it takes that place to the end
+const storedResults = testResults.slice(2);
+
+testResults;
+
+testResults.push(5.91);
+
+// slice returns a brand new array
+console.log(testResults, storedResults);
