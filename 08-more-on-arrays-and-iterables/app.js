f0ec9c20d921606759074cf127ff76b151fe209e
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-06-21 22:25:21 -0500

    added forEach

--- 08-more-on-arrays-and-iterables/app.js
+++ 08-more-on-arrays-and-iterables/app.js
@@ -80,63 +80,78 @@
 // // splice without a delete count it deletes that item and all the items after the index number.
 // hobbies.splice(1)
 
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-// // both values are changed because arrays are reference values and are updated
-// const storedResults = testResults;
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// // // both values are changed because arrays are reference values and are updated
+// // const storedResults = testResults;
 
-// // however if you slice the array then it will only ever reference the original array
-// // slice with no values give you the full array
-// const storedResults = testResults.slice();
+// // // however if you slice the array then it will only ever reference the original array
+// // // slice with no values give you the full array
+// // const storedResults = testResults.slice();
 
-// // you can include numbers in slice to take a certain range
-// // can use negative intergers
-// const storedResults = testResults.slice(0, 2);
+// // // you can include numbers in slice to take a certain range
+// // // can use negative intergers
+// // const storedResults = testResults.slice(0, 2);
 
-// // if you only put one number it takes that place to the end
-// const storedResults = testResults.slice(2);
+// // // if you only put one number it takes that place to the end
+// // const storedResults = testResults.slice(2);
 
-// concat takes an array and adds it to another unlinke push.
-// push does this [1,2,3, [4,5,6]]
-// concat does this [1,2,3,4,5,6]
-// concat returns a new array
-const storedResults = testResults.concat([3.99, 2]);
-testResults;
+// // concat takes an array and adds it to another unlinke push.
+// // push does this [1,2,3, [4,5,6]]
+// // concat does this [1,2,3,4,5,6]
+// // concat returns a new array
+// const storedResults = testResults.concat([3.99, 2]);
+// testResults;
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-// slice returns a brand new array
-console.log(testResults, storedResults);
+// // slice returns a brand new array
+// console.log(testResults, storedResults);
 
-// if you dont want the index and just want to see if the array has a value.  Returns true or false.
-console.log('includes', testResults.includes(10.99));
-// These do the same thing! Checking to see if the array inclues a value
-console.log('index of', testResults.indexOf(10.99) !== -1);
+// // if you dont want the index and just want to see if the array has a value.  Returns true or false.
+// console.log('includes', testResults.includes(10.99));
+// // These do the same thing! Checking to see if the array inclues a value
+// console.log('index of', testResults.indexOf(10.99) !== -1);
 
-// finds the index of the value you put in
-// If an array has multiple of the same value then it will only find the first one.
-// If index of does not find anything then it returns -1
-console.log(testResults.indexOf(1.5));
+// // finds the index of the value you put in
+// // If an array has multiple of the same value then it will only find the first one.
+// // If index of does not find anything then it returns -1
+// console.log(testResults.indexOf(1.5));
 
-// starts from the right and searches the array backwards.
-// If an array has multiple of the same value then it will only find the first one.
-// the Index starts with 0 as the far right. NOT the index it is on the array
-console.log(testResults.lastIndexOf(1.5));
+// // starts from the right and searches the array backwards.
+// // If an array has multiple of the same value then it will only find the first one.
+// // the Index starts with 0 as the far right. NOT the index it is on the array
+// console.log(testResults.lastIndexOf(1.5));
 
-// index of Works well with primitave values but not reference values
-// If index of does not find anything then it returns -1
-const personData = [{ name: 'Josh' }, { name: 'Josue' }];
-console.log(personData.indexOf({ name: 'Josue' }));
+// // index of Works well with primitave values but not reference values
+// // If index of does not find anything then it returns -1
+// const personData = [{ name: 'Josh' }, { name: 'Josue' }];
+// console.log(personData.indexOf({ name: 'Josue' }));
 
-const josue = personData.find((person, idx, persons) => {
-  return person.name === 'Josue';
+// const josue = personData.find((person, idx, persons) => {
+//   return person.name === 'Josue';
+// });
+
+// josue.name = 'Billy';
+
+// console.log(josue, personData);
+
+// const joshIndex = personData.findIndex((person, idx, persons) => {
+//   return person.name === 'Josh';
+// });
+
+// console.log(joshIndex);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// for (const price of prices) {
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+  taxAdjustedPrices.push(priceObj);
 });
 
-josue.name = 'Billy';
-
-console.log(josue, personData);
-
-const joshIndex = personData.findIndex((person, idx, persons) => {
-  return person.name === 'Josh';
-});
-
-console.log(joshIndex);
+console.log(prices, taxAdjustedPrices);
