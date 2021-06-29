a3c7a2430cad80871ab58cb6ddfb8409292dab09
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-06-29 10:02:00 -0500

    added split and join

--- 08-more-on-arrays-and-iterables/app.js
+++ 08-more-on-arrays-and-iterables/app.js
@@ -221,3 +221,14 @@ console.log('filtered', filteredArray);
 const sum = prices.reduce((prevValue, curValue) => prevValue + curValue, 0);
 
 console.log('sum', sum);
+
+const data = 'new york;10.99;2000';
+const transformedData = data.split(';');
+transformedData[1] = +transformedData[1];
+
+console.log('split string', transformedData);
+
+const nameFragments = ['Max', 'Schwartz'];
+const name = nameFragments.join(' ');
+
+console.log('joined', name);
