fd0e4b8a8d9e24c89562250a8cbb507f2e71ed34
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-07-05 15:17:05 -0500

    added WeakSet

--- 08-more-on-arrays-and-iterables/01-/setsmaps.js
+++ 08-more-on-arrays-and-iterables/01-/setsmaps.js
@@ -16,39 +16,53 @@
 
 //
 
-const person1 = { name: 'Max' };
-const person2 = { name: 'Manuel' };
+// const person1 = { name: 'Max' };
+// const person2 = { name: 'Manuel' };
 
-// CREATE A NEW MAP
-const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+// // CREATE A NEW MAP
+// const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
 
-console.log('person1', person1);
+// console.log('person1', person1);
 
-// ADDS DATA TO A MAP
-personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+// // ADDS DATA TO A MAP
+// personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
 
-console.log('person2', person2);
+// console.log('person2', person2);
 
-console.log('personData', personData);
-console.log('personData.get(person1)', personData.get(person1));
+// console.log('personData', personData);
+// console.log('personData.get(person1)', personData.get(person1));
 
-// Ways to print out  data
-for (const entry of personData.entries()) {
-  console.log(entry);
-}
+// // Ways to print out  data
+// for (const entry of personData.entries()) {
+//   console.log(entry);
+// }
 
-for (const [key, value] of personData.entries()) {
-  console.log(key, value);
-}
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value);
+// }
 
-// keys is a function that allows you to access only the keys
-for (const key of personData.keys()) {
-  console.log(key);
-}
+// // keys is a function that allows you to access only the keys
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
 
-// values is a function that allows you to access only the values
-for (const value of personData.values()) {
-  console.log(value);
-}
+// // values is a function that allows you to access only the values
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
 
-console.log('.size', personData.size);
+// console.log('.size', personData.size);
+
+// WEAK SET
+// Use this for data that you are ok letting go during garbage collection.
+
+let person = { name: 'Max' };
+const persons = new WeakSet();
+persons.add(person);
+
+// ... some operations
+// changing the person to null allows it to be garbage collected but we don't know the exact time it will be collected.
+person = null;
+console.log(persons);
+
+// WEAK MAP
