0d0ce147ac36f5ebaec37ca55b31b47c0da21d06
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-07-07 10:58:54 -0500

    added, deleted, and reset properties

--- 09-more-on-objects/01-/assets/scripts/objects.js
+++ 09-more-on-objects/01-/assets/scripts/objects.js
@@ -1,10 +1,44 @@
-const person = {
+// Creating an object
+// creating an object as a const you can not change the object
+
+let person = {
   name: 'Josh',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  // functions in objects are called methods
   greet: function () {
     alert('Hi there');
   }
 };
 
-person.greet();
+// WAYS TO ADD TO AN OBJECT
+// // one way to add to an object is to copy the object and rewrite it
+// // not typical
+// person = {
+//   name: 'Josh',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   // functions in objects are called methods
+//   greet: function () {
+//     alert('Hi there');
+//   },
+//   isAdmin: true
+// };
+
+// using dot properties
+person.isAdmin = true;
+
+// can also override the values in the object if it is already in the object
+person.age = 31;
+
+// // DELETE
+// // how to delete a property from an object
+// delete person.hobbies;
+// // should never do this but is possible
+// person.hobbies = undefined;
+// // null can be used but leaves the property
+// person.hobbies = null;
+
+// // Calling the method from the function
+// person.greet();
+console.log(person);
