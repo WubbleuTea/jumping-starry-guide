4eeb3c5718045ba4aa7a209554052bb511953104
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-07-12 18:09:04 -0500

    began movie application

--- 09-more-on-objects/01-/assets/scripts/objects.js
+++ 09-more-on-objects/01-/assets/scripts/objects.js
@@ -1,63 +1,31 @@
-const movieList = document.getElementById('movie-list');
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchButton = document.getElementById('search-btn');
 
-// way to get to change styles using the css names.
-movieList.style['background-color'] = 'red';
-movieList.style.display = 'block';
+const movies = [];
 
-const userChosenKeyName = 'level';
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
 
-// Creating an object
-// creating an object as a const you can not change the object
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
 
-let person = {
-  // objects can have strings as key but causes problems
-  // you normally cant acces this using person.first name
-  // you can access that with person['first name']
-  'first name': 'Josh',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  [userChosenKeyName]: '...'
-  // functions in objects are called methods
-  greet: function () {
-    alert('Hi there');
-  },
-  1.5: 'hello'
+  const newMovie = {
+    info: {
+      title, // same as title: title when the key are the same and derived from a variable}
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
 };
 
-// WAYS TO ADD TO AN OBJECT
-// // one way to add to an object is to copy the object and rewrite it
-// // not typical
-// person = {
-//   name: 'Josh',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   // functions in objects are called methods
-//   greet: function () {
-//     alert('Hi there');
-//   },
-//   isAdmin: true
-// };
-
-// using dot properties
-person.isAdmin = true;
-
-// can also override the values in the object if it is already in the object
-person.age = 31;
-
-// // DELETE
-// // how to delete a property from an object
-// delete person.hobbies;
-// // should never do this but is possible
-// person.hobbies = undefined;
-// // null can be used but leaves the property
-// person.hobbies = null;
-
-const keyName = 'first name';
-
-// // Calling the method from the function
-// person.greet();
-console.log(person);
-console.log(person[1.5]);
-console.log('keyName', person[keyName]);
-
-console.log('first name', person['first name']);
+addMovieBtn.addEventListener('click', addMovieHandler);
