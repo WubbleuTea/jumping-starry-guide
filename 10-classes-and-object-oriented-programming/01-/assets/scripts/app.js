ad6559c523db502e745d14aa3b7f9bc92f7d1ec6
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-08-01 18:56:27 -0500

    cart now adds

--- 10-classes-and-object-oriented-programming/01-/assets/scripts/app.js
+++ 10-classes-and-object-oriented-programming/01-/assets/scripts/app.js
@@ -15,9 +15,17 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, currentItem) => prevValue + currentItem.price,
+      0
+    );
+    return sum;
+  }
+
   addProduct(product) {
     this.items.push(product);
-    this.totalOutput.innerHTML = `<h2>Total \$${1}</h2>`;
+    this.totalOutput.innerHTML = `<h2>Total \$${this.totalAmount}</h2>`;
   }
 
   render() {
