c40fc99b5115965d0596ebf05ae8a0708875405a
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-08-01 18:59:59 -0500

    added setter

--- 10-classes-and-object-oriented-programming/01-/assets/scripts/app.js
+++ 10-classes-and-object-oriented-programming/01-/assets/scripts/app.js
@@ -15,6 +15,13 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
   get totalAmount() {
     const sum = this.items.reduce(
       (prevValue, currentItem) => prevValue + currentItem.price,
@@ -24,8 +31,9 @@ class ShoppingCart {
   }
 
   addProduct(product) {
-    this.items.push(product);
-    this.totalOutput.innerHTML = `<h2>Total \$${this.totalAmount}</h2>`;
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
   }
 
   render() {
